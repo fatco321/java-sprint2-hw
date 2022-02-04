f4f4a38dec11ecc77eb629e1a1e3f84f1337e185
Владимир <fatco@yandex.ru>
2022-02-04 16:21:24 +0300

    на проверку

--- src/ru/yandex/practicum/tasktraker/tasks/Task.java
+++ src/ru/yandex/practicum/tasktraker/tasks/Task.java
@@ -53,4 +53,17 @@ public class Task {
                 ", taskStatus='" + taskStatus + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(taskName, task.taskName) && Objects.equals(taskDescription, task.taskDescription) && Objects.equals(taskStatus, task.taskStatus);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(taskName, taskDescription, taskStatus);
+    }
 }
